3ae3eaa6ac8b698a8dae38e90a472581a7ae1dcc
Kwon Sung Wook[ 권성욱 ] <baw0803@linecorp.com>
2023-12-07 15:22:08 +0900

    Retest

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,11 @@
 
 # Install gems if a Gemfile exists
 if [ -e "Gemfile" ]; then
-  echo "installing ruby gems";
+  echo "installing ruby gems"
   # install bundler gem for ruby dependency management
-  gem install bundler --no-document || echo "failed to install bundle";
-  bundle config set deployment 'true';
-  bundle config path vendor/bundle;
-  bundle install || echo "failed to install bundle";
+  gem install bundler --no-document || echo "failed to install bundle"
+  bundle config set deployment 'true'
+  bundle config path vendor/bundle
+  bundle lock --add-platform x86_64-darwin-22
+  bundle install || echo "failed to install bundle"
 fi
