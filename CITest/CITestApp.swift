2bdaad29c21496b9a2d09865b8f353026443fde7
Kwon Sung Wook[ 권성욱 ] <baw0803@linecorp.com>
2023-12-07 11:40:19 +0900

    init

---
+++ CITest/CITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CITestApp.swift
+//  CITest
+//
+//  Created by SungwookKwon on 2023/12/07.
+//
+
+import SwiftUI
+
+@main
+struct CITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
